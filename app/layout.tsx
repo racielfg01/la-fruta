0f0fc938b6bba7455fcfd2acd7c9335acbc560e9
Raciel Ferrin <racielfg01@gmail.com>
2026-06-02 16:56:37 -0400

    fix: cambian el icion de la app

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,20 +30,7 @@ export const metadata: Metadata = {
   },
   manifest: '/manifest.json',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
+    icon: '/icon.svg',
     apple: '/apple-icon.png',
   },
 }
